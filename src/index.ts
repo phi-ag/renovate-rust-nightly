461d85137b6710a9a0ad18969cc9aac16c4ec876
Peter Hirn <peter.hirn@phi.ag>
2024-09-29 17:50:49 +0200

    refactor: add fetch versions function

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 class ElementHandler {
-  versions = new Set();
+  versions = new Set<string>();
 
   text({ text }: { text: string }) {
     if (text.length === 10 && text.match(/^\d{4}-\d{2}-\d{2}$/)) {
@@ -11,13 +11,18 @@ class ElementHandler {
 const sourceUrl = "https://rust-lang.github.io/rustup-components-history";
 const homepage = "https://github.com/phi-ag/renovate-rust-nightly";
 
+const fetchVersions = async (): Promise<string[]> => {
+  const res = await fetch(sourceUrl);
+  const handler = new ElementHandler();
+  await new HTMLRewriter().on("th", handler).transform(res).arrayBuffer();
+
+  return Array.from(handler.versions);
+};
+
 export default {
   async fetch(): Promise<Response> {
-    const res = await fetch(sourceUrl);
-
-    const handler = new ElementHandler();
-    await new HTMLRewriter().on("th", handler).transform(res).arrayBuffer();
-    const releases = Array.from(handler.versions).map((version) => ({ version }));
+    const versions = await fetchVersions();
+    const releases = versions.map((version) => ({ version }));
 
     return Response.json({
       releases,
