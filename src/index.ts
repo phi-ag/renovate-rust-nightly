8d7b7c44f1c875c240294d68bdd44f39669e8839
Peter Hirn <peter.hirn@phi.ag>
2025-10-30 15:32:55 +0100

    feat: add release timestamp

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,25 @@
+interface Release {
+  version: string;
+  releaseTimestamp: Date;
+}
+
 class ElementHandler {
-  readonly versions = new Set<string>();
-  readonly #versionRegex = new RegExp(/^\d{4}-\d{2}-\d{2}$/);
+  readonly releases: Release[] = [];
+  readonly #versionRegex = new RegExp(/^(?<year>\d{4})-(?<month>\d{2})-(?<day>\d{2})$/);
 
   text({ text }: { text: string }) {
-    if (text.length === 10 && this.#versionRegex.test(text)) {
-      this.versions.add(text);
+    if (text.length === 10) {
+      const releaseDate = this.#versionRegex.exec(text)?.groups;
+      if (releaseDate && releaseDate.year && releaseDate.month && releaseDate.day) {
+        const year = parseInt(releaseDate.year);
+        const month = parseInt(releaseDate.month);
+        const day = parseInt(releaseDate.day);
+
+        this.releases.push({
+          version: text,
+          releaseTimestamp: new Date(Date.UTC(year, month - 1, day))
+        });
+      }
     }
   }
 }
@@ -12,18 +27,17 @@ class ElementHandler {
 const sourceUrl = "https://rust-lang.github.io/rustup-components-history";
 const homepage = "https://github.com/phi-ag/renovate-rust-nightly";
 
-const fetchVersions = async (): Promise<string[]> => {
+const fetchReleases = async (): Promise<Release[]> => {
   const res = await fetch(sourceUrl);
   const handler = new ElementHandler();
   await new HTMLRewriter().on("th", handler).transform(res).arrayBuffer();
 
-  return Array.from(handler.versions);
+  return handler.releases;
 };
 
 export default {
   async fetch(): Promise<Response> {
-    const versions = await fetchVersions();
-    const releases = versions.map((version) => ({ version }));
+    const releases = await fetchReleases();
 
     return Response.json({
       releases,
